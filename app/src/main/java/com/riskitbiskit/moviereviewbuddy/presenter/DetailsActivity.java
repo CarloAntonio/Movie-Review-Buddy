711ca032fe30bb1e8506c4560958c7ca1744d78d
Carlo Bilbao <cbilbao88@gmail.com>
2018-01-25 21:11:44 -0800

    fix: reference the correct callback

--- app/src/main/java/com/riskitbiskit/moviereviewbuddy/presenter/DetailsActivity.java
+++ app/src/main/java/com/riskitbiskit/moviereviewbuddy/presenter/DetailsActivity.java
@@ -12,12 +12,10 @@ import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatDelegate;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TextView;
@@ -151,7 +149,7 @@ public class DetailsActivity extends AppCompatActivity implements LoaderManager.
         favesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getSupportLoaderManager().initLoader(ADD_OR_DELETE_LOADER, null, (LoaderManager.LoaderCallbacks<Cursor>) mContext);
+                getSupportLoaderManager().initLoader(ADD_OR_DELETE_LOADER, null, DetailsActivity.this);
             }
         });
 
